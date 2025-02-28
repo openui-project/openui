4f1dfa458f038229289685491cda2e6d6ea2a63f
Aaron Thompson <athompsonbriley.56@gmail.com>
2025-02-27 22:33:13 -0500

    [DEV]: Updated OpenUI's main code

--- OpenUI/OpenUI.cs
+++ OpenUI/OpenUI.cs
@@ -3,10 +3,10 @@ using System;
 namespace OpenUI
 {
     public class ViewPort
-    {
+    {   
         public ViewPort(int width, int height, string title)
         {
-            WinAPI.MessageBox(IntPtr.Zero, "Hello, World!", "WinAPI in C#", 0);
+            WinAPI.MessageBox(IntPtr.Zero, "Hello, World!", title, 0);
         }
     }
 }
