083cf5a2a397ba65d1d27314ae959d5302862b1b
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2025-03-03 22:54:58 +0000

    Add OpenUI.Graphics project and initial platform-specific classes; remove obsolete files

--- test/LocalTest/Program.cs
+++ test/LocalTest/Program.cs
@@ -1,9 +1,8 @@
-﻿using OpenUI;
+﻿using OpenUI.Graphics;
 using OpenUI.Mathematics;
 
 public static class Program {
 	public static void Main(string[] args) {
-		var view = new ViewPort(400, 300, "Simpile Example");
-		view.Run();
+		Console.WriteLine("Hello, World!");
 	}
 }
