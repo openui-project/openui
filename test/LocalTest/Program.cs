8768634ad7f9098b36bcfd659b3cbba09d6d799b
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2025-03-05 22:21:38 +0000

    [DEV]: Add support for macOS in OpenUI.Graphics and remove Android/iOS platforms

--- test/LocalTest/Program.cs
+++ test/LocalTest/Program.cs
@@ -2,7 +2,7 @@
 
 public static class Program {
 	public static void Main(string[] args) {
-		ViewPort viewPort = new ViewPort(800, 600, "Local Test");
-		viewPort.Run();
+		var view = new ViewPort(800, 600, "Simple Application");
+		view.Run();
 	}
 }
