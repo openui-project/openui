14d4c8b415623b512aa20f6b7da5ffc08dbae8fb
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2025-03-06 21:29:29 +0000

    [DEV]: Refactor project structure: remove OpenUI.Core, update .gitignore, and adjust OpenUI.Graphics project settings

--- src/OpenUI.Graphics/Platforms/Windows/OpenUI.cs
+++ src/OpenUI.Graphics/Platforms/Windows/OpenUI.cs
@@ -3,32 +3,33 @@ using System.Drawing;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 
-using Utility = OpenUI.Core.Utility;
-
-namespace OpenUI {
+namespace OpenUI
+{
     /// <summary>
 	/// The <c>GameWindow</c> class represents the main window of the application, acting as the primary container 
 	/// for rendering and user interaction. It manages the window's size, position, and overall layout, handling 
 	/// events such as resizing, input handling, and refreshing the display. This class provides the foundation 
 	/// for rendering graphical content and serves as the central interface between the application and the user.
 	/// </summary>
-    public class GameWindow {
+    public class GameWindow
+    {
         private Form form;
 
-        public GameWindow(int width, int height, string title) {
+        public GameWindow(int width, int height, string title)
+        {
             form = new Form
-			{
-				ClientSize = new Size(width, height),
-				Text = title,
-				BackColor = Color.Black
-		    };
+            {
+                ClientSize = new Size(width, height),
+                Text = title,
+                BackColor = Color.Black,
+                StartPosition = FormStartPosition.CenterScreen,
+            };
         }
 
-        public void Show() {
-            Utility.TraceLog(LogLevel.Info, "Starting application...");
-			form.Show();
+        public void Show()
+        {
+            form.Show();
             Application.Run(form);
-            Utility.TraceLog(LogLevel.Info, "Application terminated.");
         }
     }
 }
