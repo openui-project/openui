e0e54de26eef082a3c076f93adf06e23c7ba9ec5
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2025-03-11 22:56:58 +0000

    [DEV]: Remove Vector2 class and update project file; add Texture and Texture2D structs for texture handling

--- src/OpenUI.Graphics/Platforms/Windows/OpenUI.cs
+++ src/OpenUI.Graphics/Platforms/Windows/OpenUI.cs
@@ -5,6 +5,52 @@ using System.Runtime.InteropServices;
 
 namespace OpenUI
 {
+    internal interface ITexture
+    {
+        int Width { get; }
+        int Height { get; }
+    }
+
+    public struct Texture : ITexture
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        private Bitmap Image { get; set; }
+
+        public Texture(string filePath)
+        {
+            try
+            {
+                Image = new Bitmap(filePath); 
+                Width = Image.Width;
+                Height = Image.Height;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error loading texture: {ex.Message}");
+            }
+        }
+    }
+
+    public struct Texture2D : ITexture
+    {
+        private Texture _texture;
+
+        public int Width
+        {
+            get { return _texture.Width; }
+        }
+
+        public int Height
+        {
+            get { return _texture.Height; }
+        }
+
+        public Texture2D(string filePath) {
+            _texture = new Texture(filePath);
+        }
+    }
+
     /// <summary>
 	/// The <c>GameWindow</c> class represents the main window of the application, acting as the primary container 
 	/// for rendering and user interaction. It manages the window's size, position, and overall layout, handling 
@@ -15,6 +61,8 @@ namespace OpenUI
     {
         private Form form;
 
+        public Action<PaintEventArgs>? Paint;
+
         public GameWindow(int width, int height, string title)
         {
             form = new Form
@@ -24,16 +72,18 @@ namespace OpenUI
                 BackColor = Color.Black,
                 StartPosition = FormStartPosition.CenterScreen,
             };
+            form.Paint += GameWindow_Paint;
         }
 
-        public Mathematics.Vector2 GetWindowPosition() {
-            return new Mathematics.Vector2(form.Left, form.Top);
-        }
         public void Show()
         {
             Application.EnableVisualStyles();
             form.Show();
             Application.Run(form);
+        }
+
+        private void GameWindow_Paint(object sender, PaintEventArgs e) {
+            Paint?.Invoke(e);
         }
     }
 }
