e12575beb3b9256eaa6bc7f95b88b3c599c87117
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2025-03-11 20:15:24 -0400

    Update OpenUI.cs

--- src/OpenUI.Graphics/Platforms/Windows/OpenUI.cs
+++ src/OpenUI.Graphics/Platforms/Windows/OpenUI.cs
@@ -1,56 +1,47 @@
 using System;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 
 namespace OpenUI
 {
-    internal interface ITexture
-    {
-        int Width { get; }
-        int Height { get; }
-    }
+    internal interface ITexture {
+			int Width { get; }
+			int Height { get; }
+			Bitmap Image { get; }
+		}
 
-    public struct Texture : ITexture
-    {
-        public int Width { get; private set; }
-        public int Height { get; private set; }
-        private Bitmap Image { get; set; }
+		public struct Texture : ITexture {
+			public int Width { get; private set; }
+			public int Height { get; private set; }
+			public Bitmap Image { get; private set; }
 
-        public Texture(string filePath)
-        {
-            try
-            {
-                Image = new Bitmap(filePath); 
-                Width = Image.Width;
-                Height = Image.Height;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error loading texture: {ex.Message}");
-            }
-        }
-    }
+			public Texture(string filePath) {
+				try
+				{
+					Image = new Bitmap(filePath); 
+					Width = Image.Width;
+					Height = Image.Height;
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine($"Error loading texture: {e.Message}");
+				}
+			}
+		}
 
-    public struct Texture2D : ITexture
-    {
-        private Texture _texture;
+		public struct Texture2D : ITexture {
+			private Texture _texture;
 
-        public int Width
-        {
-            get { return _texture.Width; }
-        }
+			public int Width => _texture.Width;
+			public int Height => _texture.Height;
+			public Bitmap Image => _texture.Image;
 
-        public int Height
-        {
-            get { return _texture.Height; }
-        }
-
-        public Texture2D(string filePath) {
-            _texture = new Texture(filePath);
-        }
-    }
-
+			public Texture2D(string filePath) {
+				_texture = new Texture(filePath);
+			}
+		}
     /// <summary>
 	/// The <c>GameWindow</c> class represents the main window of the application, acting as the primary container 
 	/// for rendering and user interaction. It manages the window's size, position, and overall layout, handling 
@@ -72,7 +63,40 @@ namespace OpenUI
                 BackColor = Color.Black,
                 StartPosition = FormStartPosition.CenterScreen,
             };
-            form.Paint += GameWindow_Paint;
+            form.Paint += OnPaint;
+        }
+
+        public void DrawTexture(Texture2D texture, int posX, int posY, Color tint)
+        {
+            if (texture.Width > 0 && texture.Height > 0)
+            {
+                using (var image = texture.Image)
+                {
+                    var tintedImage = new Bitmap(image);
+                    using (Graphics g2d = Graphics.FromImage(tintedImage))
+                    {
+                        var tintMatrix = new ColorMatrix(new[]
+                        {
+                            new float[] { tint.R / 255f, 0, 0, 0, 0 },
+                            new float[] { 0, tint.G / 255f, 0, 0, 0 },
+                            new float[] { 0, 0, tint.B / 255f, 0, 0 },
+                            new float[] { 0, 0, 0, 1f, 0 },
+                            new float[] { 0, 0, 0, 0, 1f }
+                        });
+                        
+                        var imageAttributes = new ImageAttributes();
+                        imageAttributes.SetColorMatrix(tintMatrix);
+                        g2d.DrawImage(tintedImage, new Rectangle(0, 0, image.Width, image.Height), 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, imageAttributes);
+                    }
+                    // Draw the tinted image at the specified position.
+                    form.CreateGraphics().DrawImage(tintedImage, posX, posY);
+                }
+            }
+        }
+
+        public void DrawTexture(Graphics g, Texture2D texture, int posX, int posY, Color tint) 
+        {
+            return;
         }
 
         public void Show()
@@ -82,7 +106,7 @@ namespace OpenUI
             Application.Run(form);
         }
 
-        private void GameWindow_Paint(object sender, PaintEventArgs e) {
+        private void OnPaint(object sender, PaintEventArgs e) {
             Paint?.Invoke(e);
         }
     }
