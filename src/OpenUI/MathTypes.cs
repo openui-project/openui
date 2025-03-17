d414cb42ee179f5b6e1238792fac99493d03aab8
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2025-03-17 22:04:07 +0000

    Refactor Vector2 struct to use properties for X and Y, and improve GetHashCode implementation

--- src/OpenUI/MathTypes.cs
+++ src/OpenUI/MathTypes.cs
@@ -2,30 +2,32 @@ using System;
 using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
 
-using System;
-using System.Runtime.InteropServices;
-using System.Runtime.CompilerServices;
-
-namespace OpenUI {
+namespace OpenUI
+{
     [Serializable]
     [StructLayout(LayoutKind.Sequential)]
-    public struct Vector2 
+    public struct Vector2
     {
-        public float X, Y;
+        public float X { get; set; }
+        public float Y { get; set; }
 
-        public Vector2(float x, float y) {
-            X = x;
-            Y = y;
-        }
+        public Vector2(float x, float y) { X = x; Y = y; }
 
-        public static readonly Vector2 Zero = new Vector2(0, 0);
+        // Methods
 
+        // Predefined vectors
+
+        // Properties & Fields
         public static int SizeInBytes { get; } = Unsafe.SizeOf<Vector2>();
 
-        public static bool operator ==(Vector2 v1, Vector2 v2) =>  v1.X == v2.X && v1.Y == v2.Y;
+        // Conversion Methods
+
+        // Operators
+        public static bool operator ==(Vector2 v1, Vector2 v2) => v1.X == v2.X && v1.Y == v2.Y;
         public static bool operator !=(Vector2 v1, Vector2 v2) => !(v1 == v2);
 
+        // Overriden Methods
         public override string ToString() => $"({X}, {Y})";
-        public override int GetHashCode() => (X, Y).GetHashCode();
+        public override int GetHashCode() => HashCode.Combine(X, Y);
     }
 }
