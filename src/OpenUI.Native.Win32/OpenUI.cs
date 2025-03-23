a24bd7bbaa4796a19444ea127c1595652af2ed3f
Aaron Thompson <athompsonbriley.56@gmail.com>
2025-03-22 20:51:56 -0400

    [DEV]: Updated OpenUI.cs in OpenUI.Native.Win32

--- src/OpenUI.Native.Win32/OpenUI.cs
+++ src/OpenUI.Native.Win32/OpenUI.cs
@@ -4,7 +4,7 @@ using System.Windows.Forms;
 
 namespace OpenUI
 {
-	/// <summary>
+  /// <summary>
   /// The <c>GameWindow</c> class represents the main window of the application, acting as the primary container 
   /// for rendering and user interaction. It manages the window's size, position, and overall layout, handling 
   /// events such as resizing, input handling, and refreshing the display. This class provides the foundation 
